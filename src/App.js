08bedfd77bce2aafbd0ba29dbb4cb3a64d4b84a4
Abel AR <abel_john_14@hotmail.com>
2021-12-21 21:38:01 -0500

    Add styles to the Login form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUpForm from './components/SignUpForm/SignUpForm';
+import './App.css';
 
 function App() {
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,55 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+import { BsCoin } from "react-icons/bs"
 
 const Login = () => {
     return (
-        <div className="container">
-            <Form>
-                <Form.Group className="mb-3 b-flex-column" controlId="formBasicUser">
-                    <Form.Label>User ID</Form.Label>
-                    <Form.Control type="text" placeholder="user"/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="password"/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Login
-                </Button>
-                <Link to="/signup" className="signup">sign up</Link>
-            </Form>
+        // <div className="container-sm border p-2 border-white border-2 bg-warning bg-opacity-75 rounded-3">
+        <div className="container-login">
+            <div className="login">
+                <div className="login__title">
+                    <p className="text-capitalize fw-bolder lh-base fs-1 login__title--name">my wallet</p>
+                    <BsCoin size="32px" color="#FFFFFF"/>
+                </div>
+                <form className="form">
+                    <div className="login__form">
+                        <div className="mb-3">
+                            <label 
+                                htmlFor="exampleInputUser1" 
+                                className="form-label text-capitalize fw-bolder text-white fs-5 res"
+                            >username</label>
+                            <input 
+                                type="text" 
+                                className="form-control border-white border-2" 
+                                id="exampleInputUser1" 
+                                aria-describedby="emailHelp" 
+                                placeholder="Username"
+                            />
+                        </div>
+
+                        <div className="mb-3">
+                            <label 
+                                htmlFor="inputPassword6" 
+                                className="form-label text-white fw-bolder text-capitalize fs-5 res"
+                            >password</label>
+                            <input 
+                                type="password" 
+                                className="form-control border-white border-2" 
+                                id="inputPassword6" 
+                                placeholder="Password"
+                            />
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-center buttons">    
+                        <button 
+                            type="submit" 
+                            className="btn btn-warning fw-bolder text-capitalize lh-base align-middle border-2 border-light text-white"
+                        >login</button>
+                        <Link to="/signup" className="btn btn-warning fw-bolder text-capitalize lh-base align-middle border-2 border-light text-white signup">sign up</Link>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
