1b6b6c83a25ea095cf8440657563e886caebdcb1
Abel AR <abel_john_14@hotmail.com>
2021-12-22 02:10:26 -0500

    Add field phone to the sign up form and Add states to the forms

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 import { BsCoin } from "react-icons/bs"
 
 const Login = () => {
+    const [login, setLogin] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleChangeUsername = (e) => {
+        setLogin({ ...login, username: e.target.value });
+    }
+
+    const handleChangePassword = (e) => {
+        setLogin({ ...login, password: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLogin({...login});
+        setLogin({
+            username: '',
+            password: '',
+        })
+    }
+
     return (
         // <div className="container-sm border p-2 border-white border-2 bg-warning bg-opacity-75 rounded-3">
         <div className="container-login">
@@ -12,7 +34,7 @@ const Login = () => {
                     <p className="text-capitalize fw-bolder lh-base fs-1 login__title--name">my wallet</p>
                     <BsCoin size="32px" color="#FFFFFF"/>
                 </div>
-                <form className="form">
+                <form className="form" onSubmit={handleSubmit}>
                     <div className="login__form">
                         <div className="mb-3">
                             <label 
@@ -24,6 +46,8 @@ const Login = () => {
                                 className="form-control border-white border-2" 
                                 id="user"
                                 placeholder="Username"
+                                value={login.username}
+                                onChange={handleChangeUsername}
                             />
                         </div>
 
@@ -37,6 +61,8 @@ const Login = () => {
                                 className="form-control border-white border-2" 
                                 id="password" 
                                 placeholder="Password"
+                                value={login.password}
+                                onChange={handleChangePassword}
                             />
                         </div>
                     </div>
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,50 +1,170 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 import "./SignUpForm.css";
 
 const SignUpForm = () => {
+    const [fieldPhone, setFieldPhone] = useState('');
+    const [state, setstate] = useState({
+        username: '',
+        password: '',
+        countryCode: '',
+        firstName: '',
+        lastName: '',
+        address: '',
+        birthdate: '',
+        date: new Date(),
+        status: 1,
+    });
+
+    const [form, setForm] = useState({})
+
+    const onChangeUsername = (e) => {
+        setstate({...state, username: e.target.value});
+    }
+
+    const onChangePassword = (e) => {
+        setstate({...state, password: e.target.value});
+    }
+
+    const onChangeCountryCode = (e) => {
+        setstate({...state, countryCode: e.target.value});
+    }
+
+    const onChangeFirstName = (e) => {
+        setstate({...state, firstName: e.target.value});
+    }
+
+    const onChangeLastName = (e) => {
+        setstate({...state, lastName: e.target.value});
+    }
+
+    const onChangeAddress = (e) => {
+        setstate({...state, address: e.target.value});
+    }
+
+    const onChangeBirthdate = (e) => {
+        setstate({...state, birthdate: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
+        setForm({ ...form, ...state, fieldPhone });
+        console.log(form);
+
+        setFieldPhone('')
+        setstate({
+            username: '',
+            password: '',
+            countryCode: '',
+            firstName: '',
+            lastName: '',
+            address: '',
+            birthdate: '',
+            date: new Date(),
+            status: 1,
+        });
+    }
+
     return (
         <div className="container-login">
             <div className="sign-up">
                 <p className="text-capitalize fw-bolder text-white lh-base fs-1">sign up</p>
-                <form className="form">
+                <form className="form" onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="user" className="form-label text-capitalize fw-bold text-white">username</label>
-                        <input type="text" className="form-control" id="user" />
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="user" 
+                            name="username"
+                            value={state.username}
+                            onChange={onChangeUsername}
+                        />
                     </div>
                     
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label text-capitalize fw-bold text-white">password</label>
-                        <input type="password" className="form-control" id="password" />
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            id="password" 
+                            name="password"
+                            value={state.password}
+                            onChange={onChangePassword}
+                        />
                     </div>
                     
                     <div className="mb-3">
                         <label htmlFor="countryCode" className="form-label text-capitalize fw-bold text-white">country code</label>
-                        <input type="text" className="form-control" id="countryCode" />
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="countryCode" 
+                            name="countryCode"
+                            value={state.countryCode}
+                            onChange={onChangeCountryCode}
+                        />
                     </div>
                     
                     <div className="mb-3">
                         <label htmlFor="phone" className="form-label text-capitalize fw-bold text-white">phone</label>
-                        <input type="text" className="form-control" id="phone" />
+                        <PhoneInput 
+                            className="form-control phone-input" 
+                            id="phone" 
+                            name="phone"
+                            value={fieldPhone}
+                            onChange={setFieldPhone}
+                        /> 
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="firstName" className="form-label text-capitalize fw-bold text-white">first name</label>
-                        <input type="text" className="form-control" id="firstName" />
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="firstName" 
+                            name="firstName"
+                            value={state.firstName}
+                            onChange={onChangeFirstName}
+                        />
                     </div>
                     
                     <div className="mb-3">
                         <label htmlFor="lastName" className="form-label text-capitalize fw-bold text-white">last name</label>
-                        <input type="text" className="form-control" id="lastName" />
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="lastName" 
+                            name="lastName"
+                            value={state.lastName}
+                            onChange={onChangeLastName}
+                        />
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="address" className="form-label text-capitalize fw-bold text-white">address</label>
-                        <input type="text" className="form-control" id="address" />
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="address" 
+                            name="address"
+                            value={state.address}
+                            onChange={onChangeAddress}
+                        />
                     </div>
                     
                     <div className="mb-3">
                         <label htmlFor="birthdate" className="form-label text-capitalize fw-bold text-white">birthdate</label>
-                        <input type="date" className="form-control" id="birthdate" />
+                        <input 
+                            type="date" 
+                            className="form-control" 
+                            id="birthdate" 
+                            name="birthdate"
+                            value={state.birthdate}
+                            onChange={onChangeBirthdate}
+                        />
                     </div>
 
                     <div className="d-flex justify-content-center">
